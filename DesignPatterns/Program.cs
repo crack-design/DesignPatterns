ee35a712edc764fceab8727c5d485c558b38229c
Taras Vytrykush <crack.design@gmail.com>
2017-11-19 18:14:43 +0200

    Added Adapter pattern

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using DesignPatterns.Command;
 using DesignPatterns.Command.SecondImplementation;
+using DesignPatterns.Adapter;
 
 namespace DesignPatterns
 {
@@ -9,30 +10,39 @@ namespace DesignPatterns
         static void Main(string[] args)
         {
 
-#region Command
+            #region Command
 
             //Client client = new Client();
             //client.Compute('+', 20, 150);
             //Console.ReadLine();
             #endregion
 
-#region Command - via interface
+            #region Command - via interface
 
-            Receiver receiverObj = new Receiver();
+            //Receiver receiverObj = new Receiver();
 
-            ICommand command = new ConcreteCommand(receiverObj, "Johny");
+            //ICommand command = new ConcreteCommand(receiverObj, "Johny");
 
-            var invokerObj = new Command.SecondImplementation.Invoker();
+            //var invokerObj = new Command.SecondImplementation.Invoker();
 
-            invokerObj.SetCommand(command);
+            //invokerObj.SetCommand(command);
 
-            invokerObj.ExecuteCommand();
+            //invokerObj.ExecuteCommand();
 
-            Console.Read();
+            //Console.Read();
 
             #endregion
             //Запитати про дженерік інтерфейс для команди
             //https://www.cuttingedge.it/blogs/steven/pivot/entry.php?id=91
+            #region Adapter
+
+            var newSocket = new NewSocket();
+            AdapterConsumer.Charge(newSocket);
+            var oldSocket = new OldSocket();
+            var adapter = new Adapter.Adapter(oldSocket);
+            AdapterConsumer.Charge(adapter);
+            Console.ReadLine();
+#endregion
         }
     }
 }
